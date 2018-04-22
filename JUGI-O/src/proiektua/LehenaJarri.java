1d70eaa00f4a3e69a7b6f4115779d752a9e652b9
Gorka <Gorka@DESKTOP-F92V5C7>
2018-04-22 18:48:11 +0200

    aa

--- JUGI-O/src/proiektua/LehenaJarri.java
+++ JUGI-O/src/proiektua/LehenaJarri.java
@@ -5,13 +5,8 @@ public class LehenaJarri implements AurreratuPortaera {
 	}
 	
 	public void aurreratu(int b, int h) {			// b : posicion donde termina, h : posicion donde esta ahora
-		
-		
 		Karta k = Tableroa.getTableroa().getMahaiKartak().kenduKartaPos(h);	
-		
-		Tableroa.getTableroa().getMahaiKartak().gehituHasieran(k);
-		
-		
+		Tableroa.getTableroa().getMahaiKartak().getLista().add(0, k);	
 	}
 }
 
