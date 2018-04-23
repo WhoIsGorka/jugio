2f87a7bdf5e2abe188955f498f4d65e81a339165
Julen Rojo <Julen Rojo@10.109.241.214>
2018-04-23 19:30:46 +0200

    kamalehoia

--- JUGI-O/src/proiektua/Kamalehoia.java
+++ JUGI-O/src/proiektua/Kamalehoia.java
@@ -1,18 +1,32 @@
 package proiektua;
 
+import javax.swing.JOptionPane;
+
 public class Kamalehoia extends Karta {
 
 	public Kamalehoia(int pId,String pIzena,String pDeskr,boolean pErre,int pPuntuak,String pIrudia,String pKolorea){
 		super(pId, pIzena, pDeskr, pErre, pPuntuak, pIrudia, pKolorea);
 	}
 	public void animaladaEgin(){
+		MahaiKartak mk = Tableroa.getTableroa().getMahaiKartak();
+		String kolorea;
+		if(this.getKolorea()=="Urdina"){
+			kolorea= "Urdina";
+		}else{
+			kolorea="Berdea";
+		}
+		int zenb =  Integer.parseInt(JOptionPane.showInputDialog(null, "Aukeratu mahiko animali bat (0-4): "));
+		Karta animaliKopia= mk.getKarta(zenb);
+		animaliKopia.setKolorea(kolorea);
 		
-		// Ze animaliatan bihurtuko den galdetu
 		
-		// Kamalehoia animali hori bihurtu
+		int pos = mk.getPosizioa(this.getIzena(),this.getKolorea());
+		mk.getLista().set(pos, animaliKopia);
 		
-		// Animali berriaren animalada egin
 		
-		// Animali berria kamalehoia bihurtu
+		animaliKopia.animaladaEgin();
+		pos = mk.getPosizioa(animaliKopia.getIzena(),animaliKopia.getKolorea());
+		
+		mk.getLista().set(pos, this);
 	}
 }
