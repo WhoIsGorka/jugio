e1a8276efd485abd37b8724b0d8259ada5d1ad65
Gorka <Gorka@DESKTOP-F92V5C7>
2018-05-08 17:37:26 +0200

    ggg

--- JUGI-O/src/proiektua/Loroa.java
+++ JUGI-O/src/proiektua/Loroa.java
@@ -13,22 +13,38 @@ public class Loroa extends Karta {
 		bPortaera=new BotaBat();
 	}
 	public void animaladaEgin(){
-		String kolorea;
-		if(this.getKolorea()=="Urdina"){
-			kolorea= "Urdina";
-		}else{
-			kolorea="Berdea";
-		}
-		
-		if(kolorea=="Urdina"){
-			int zenb =  Integer.parseInt(JOptionPane.showInputDialog(null, "Sartu kalera bota nahi duzun kartaren posizioa (1 -"+Tableroa.getTableroa().getMahaiKartak().getTamaina()+"):"));
-			Karta animaliKopia= Tableroa.getTableroa().getMahaiKartak().getKarta(zenb-1);	
-			bPortaera.bota(animaliKopia.getIzena(), animaliKopia.getKolorea());}
-		else if(kolorea=="Berdea"){
-			int zenb = ThreadLocalRandom.current().nextInt(1, Tableroa.getTableroa().getMahaiKartak().getTamaina()+1);
-			Karta animaliKopia= Tableroa.getTableroa().getMahaiKartak().getKarta(zenb-1);	
-			bPortaera.bota(animaliKopia.getIzena(), animaliKopia.getKolorea());
-			
-		}
-		
-}}
+		boolean ondo=false;
+		do {
+			try{
+				String kolorea;
+				if(this.getKolorea()=="Urdina"){
+					kolorea= "Urdina";
+				}else{
+					kolorea="Berdea";
+				}
+				
+				if(kolorea=="Urdina"){
+					int zenb =  Integer.parseInt(JOptionPane.showInputDialog(null, "Sartu kalera bota nahi duzun kartaren posizioa (1-"+(Tableroa.getTableroa().getMahaiKartak().getTamaina()-1)+"):"));
+					if (zenb <= 0 || zenb > (Tableroa.getTableroa().getMahaiKartak().getTamaina()-1)) {
+						throw new KamaLoroException();
+					}
+					else {
+					Karta animaliKopia= Tableroa.getTableroa().getMahaiKartak().getKarta(zenb-1);	
+					bPortaera.bota(animaliKopia.getIzena(), animaliKopia.getKolorea());
+					ondo=true;
+					}
+				}
+				else if(kolorea=="Berdea"){
+					int zenb = ThreadLocalRandom.current().nextInt(1, Tableroa.getTableroa().getMahaiKartak().getTamaina()+1);
+					Karta animaliKopia= Tableroa.getTableroa().getMahaiKartak().getKarta(zenb-1);	
+					bPortaera.bota(animaliKopia.getIzena(), animaliKopia.getKolorea());
+					ondo=true;		
+				}
+			}catch (KamaLoroException e) {
+				JOptionPane.showMessageDialog(null, e.mezua());
+			}catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "Zenbaki bat sartu behar da");
+			}
+		}while(!ondo);
+	}
+}
