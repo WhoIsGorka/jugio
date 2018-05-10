18d023b613cca3808e0f4d15fa52c240a62da3ce
IMALOL <bisbinido@gmail.com>
2018-05-10 11:29:07 +0200

    jyhhhh

--- JUGI-O2/src/proiektua/Mysql.java
+++ JUGI-O2/src/proiektua/Mysql.java
@@ -12,42 +12,43 @@ public class Mysql {
 		jolastu();
 	}
 	
-	public static void jolastu() throws ClassNotFoundException, SQLException{
+	public static Connection jolastu() throws ClassNotFoundException, SQLException{
 		String zerbitzaria = "jdbc:mysql://localhost:3306/barbestial";
 		String erabiltzailea = "root";
 		String pasahitza = "";
 		Connection konexioa = (Connection) DriverManager.getConnection(zerbitzaria, erabiltzailea, pasahitza);
-		
-		Scanner sc = null;
-		int zenb=0;
+		return konexioa;
+		}
+	public void sartuNanIzenaAbizena(String pNan, String pIzena, String pAbizena)throws ClassNotFoundException, SQLException{
+		Connection konexioa= jolastu();
+		Class.forName("com.mysql.jdbc.Driver");
+		Statement s = (Statement) konexioa.createStatement();	
+		s.executeUpdate("INSERT INTO jokalari VALUES('"+pNan+"','"+pIzena+"','"+pAbizena+"')");
+	} 
+	public void sartuIzenaJokalaria(String pIzena)throws ClassNotFoundException, SQLException{
+		Connection konexioa= jolastu();
 		Class.forName("com.mysql.jdbc.Driver");
 		Statement s = (Statement) konexioa.createStatement();
-		
-		zenb = sc.nextInt();
-		switch (zenb) {
-		case 1:
-			System.out.println("Sartu jokalariaren izena: ");
-			String izena = sc.nextLine();
-			System.out.println("Sartu jokalariaren abizena:");
-			String abizena = sc.nextLine();
-			System.out.println("Sartu jokalariaren NAN:");
-			String nan = sc.nextLine(); 
-			s.executeUpdate("INSERT INTO jokalari VALUES('"+nan+"','"+izena+"','"+abizena+"')");
-			break;
-		case 2:
-			System.out.println("Sartu jokalariaren izena: ");
-			izena = sc.nextLine();
-			s.executeUpdate("INSERT INTO partida(jokIzen) VALUES ('"+izena+"')"); 
-			break;
-		case 3:
-			s.executeUpdate("select 'izena' from 'jokalari'");
-			break;
-		case 4:
-			konexioa.close();
-			break;
-		}
+		s.executeUpdate("INSERT INTO partida(jokIzen) VALUES ('"+pIzena+"')");
 	}
-}
+	public void erakutsiJokalariIzena()throws ClassNotFoundException, SQLException{
+		Connection konexioa= jolastu();
+		Class.forName("com.mysql.jdbc.Driver");
+		Statement s = (Statement) konexioa.createStatement();	
+		s.executeUpdate("select 'izena' from 'jokalari'");}
+
+	public void konexioaZarratu()throws ClassNotFoundException, SQLException{
+		Connection konexioa= jolastu();
+		Class.forName("com.mysql.jdbc.Driver");
+		konexioa.close();
+	}
+	public void rankingErakutsi()throws ClassNotFoundException, SQLException{
+		Connection konexioa= jolastu();
+		Class.forName("com.mysql.jdbc.Driver");
+		Statement s = (Statement) konexioa.createStatement();	
+		s.executeUpdate("select * from ranking");}
+	}
+	
 
 
 
