3a7f7abb58c428013ab93dcf9a51798d7ff54cf1
Julen Rojo <Julen Rojo@10.109.241.214>
2018-05-20 18:07:47 +0200

    retraso

--- JUGI-O2/src/proiektua/Tableroa.java
+++ JUGI-O2/src/proiektua/Tableroa.java
@@ -96,33 +96,33 @@ public class Tableroa extends Observable{
 		
 		this.m1 = new EskuKartak();
 		
-		//this.m1.gehituKarta(mofeta1);
+		this.m1.gehituKarta(mofeta1);
 		this.m1.gehituKarta(loroa1);
 		this.m1.gehituKarta(kanguru1);
-		//this.m1.gehituKarta(tximinoa1);
+		this.m1.gehituKarta(tximinoa1);
 		this.m1.gehituKarta(kamalehoia1);
-		//this.m1.gehituKarta(itsaslehoia1);
-		//this.m1.gehituKarta(zebra1);
-		//this.m1.gehituKarta(jirafa1);
-		//this.m1.gehituKarta(sugea1);
-		//this.m1.gehituKarta(kokodriloa1);
-		//this.m1.gehituKarta(hipopotamoa1);
-		//this.m1.gehituKarta(lehoia1);
+		this.m1.gehituKarta(itsaslehoia1);
+		this.m1.gehituKarta(zebra1);
+		this.m1.gehituKarta(jirafa1);
+		this.m1.gehituKarta(sugea1);
+		this.m1.gehituKarta(kokodriloa1);
+		this.m1.gehituKarta(hipopotamoa1);
+		this.m1.gehituKarta(lehoia1);
 		
 		this.m2 = new EskuKartak();
 		
-		/*this.m2.gehituKarta(mofeta2);
+		this.m2.gehituKarta(mofeta2);
 		this.m2.gehituKarta(loroa2);
 		this.m2.gehituKarta(kanguru2);
-		this.m2.gehituKarta(tximinoa2);*/
+		this.m2.gehituKarta(tximinoa2);
 		this.m2.gehituKarta(kamalehoia2);
-	/*	this.m2.gehituKarta(itsaslehoia2);
+		this.m2.gehituKarta(itsaslehoia2);
 		this.m2.gehituKarta(zebra2);
 		this.m2.gehituKarta(jirafa2);
 		this.m2.gehituKarta(sugea2);
 		this.m2.gehituKarta(kokodriloa2);
 		this.m2.gehituKarta(hipopotamoa2);
-		this.m2.gehituKarta(lehoia2);*/
+		this.m2.gehituKarta(lehoia2);
 	}
 	public void hasieraketak(){
 		this.ilara = new MahaiKartak();
